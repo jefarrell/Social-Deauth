58e752c9379bb498cb8a0ead1676019b5648cf90
John Farrell <johnfarrell@128-122-6-152.DYNAPOOL.NYU.EDU>
2015-03-26 18:46:18 -0400

    comments

--- deauth.py
+++ deauth.py
@@ -1,14 +1,18 @@
+# Testing a python version of Julian Oliver's "Social Deauth" script
+# http://julianoliver.com/output/log_2013-02-12_16-55
+# Designed to run on a Raspberry Pi
+# Requires arp-scan and aireplay-ng
 
 import subprocess
 import os
-MACs = []
+
 
 def testing():
     try:
             make_mon = subprocess.call(['sudo','airmon-ng','start','mon0','11'])
             scan = subprocess.check_output(['sudo','arp-scan','--localnet','--interface=mon0'])
-            nscan = scan.split("\t")
-            print nscan[1::2]
+            tscan = scan.split("\t")
+            print tscan[1::2]
     except Exception, e:
             print "Error:", e
 
